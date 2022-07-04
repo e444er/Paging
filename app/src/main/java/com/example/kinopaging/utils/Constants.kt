c1b8b9a2f007fefc8d04c6b3a66c4ab94c158117
e444er <e444er@gmail.com>
2022-07-04 18:28:39 +0600

    finish

--- app/src/main/java/com/example/kinopaging/utils/Constants.kt
+++ app/src/main/java/com/example/kinopaging/utils/Constants.kt
@@ -3,7 +3,7 @@ package com.example.kinopaging.utils
 
 object Constants {
 
-    const val BASE_URL: String = "https://rickandmortyapi.com/api/"
     const val END_POINT: String = "character"
 
+    const val BASE_URL: String = "https://rickandmortyapi.com/api/"
 }
